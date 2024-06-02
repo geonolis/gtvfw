922987b43341d6cfdc1bf73b6996b1d3f6bfbb54
geonolis <geonolis@hotmail.com>
2024-06-02 17:47:13 +0300

    Squashed commit of the following:
    commit 230c002afc4d15ea7bd3513d3257985348d8db18
    Author: geonolis <geonolis@hotmail.com>
    Date:   Sun Jun 2 17:17:30 2024 +0300
    
        minor css
    
    commit daa91581eb110f75232405b3e301346992c20f7c
    Author: geonolis <geonolis@hotmail.com>
    Date:   Sun Jun 2 17:15:52 2024 +0300
    
        minor css change
    
    commit 2ac57adc7b8a1b67cfc11c9469868b2627042132
    Author: geonolis <geonolis@hotmail.com>
    Date:   Sun Jun 2 17:06:19 2024 +0300
    
        xhange css images url
    
    commit 0451af80e29abfc1c0523e784ba8b7919bac5677
    Author: geonolis <geonolis@hotmail.com>
    Date:   Sun Jun 2 17:03:09 2024 +0300
    
        Update class-create-geniki-taxydromiki-vouchers-for-woo-v3-public.php
    
    commit c5168415ca251273944721d8b50ca3f664b7ddd7
    Author: geonolis <geonolis@hotmail.com>
    Date:   Sun Jun 2 17:02:10 2024 +0300
    
        trying to load css to order page
    
    commit 6d956916d843cf4a66bdfada417a610025f0a22e
    Author: geonolis <geonolis@hotmail.com>
    Date:   Sun Jun 2 16:52:32 2024 +0300
    
        fix error
    
    commit ed81750697ed86f2d4ae125d805adf4009e06734
    Author: geonolis <geonolis@hotmail.com>
    Date:   Sun Jun 2 16:50:20 2024 +0300
    
        trying to fix acount page css
    
    commit dde95b0834aa5ce08b11cac22016fe75a5d1d931
    Author: geonolis <geonolis@hotmail.com>
    Date:   Sun Jun 2 16:34:41 2024 +0300
    
        fix public css file location
    
    commit 5710ed8d666420e982e273d95f94862056f69ade
    Author: geonolis <geonolis@hotmail.com>
    Date:   Sun Jun 2 16:29:27 2024 +0300
    
        Fix errors client account track shipping
    
    commit 3865eb2a867f247dd4fc692bbde2afa3db40714a
    Author: geonolis <geonolis@hotmail.com>
    Date:   Sun Jun 2 00:11:43 2024 +0300
    
        css admin enqueue fix
    
    commit d3e1421e7bf3cff1d5a34a42b2afaad900a19b38
    Merge: e53bcd3 a608f3f
    Author: geonolis <geonolis@hotmail.com>
    Date:   Sat Jun 1 23:59:55 2024 +0300
    
        Merge branch 'master' into developing
    
    commit e53bcd31341b7fbbb185d9af33042f337262e30f
    Author: geonolis <geonolis@hotmail.com>
    Date:   Sat Jun 1 21:01:02 2024 +0300
    
        fix error
    
    commit 24da3cf15e91ad2cf5e6dfda78085c1421d753eb
    Author: geonolis <geonolis@hotmail.com>
    Date:   Sat Jun 1 20:46:46 2024 +0300
    
        error fix
    
    commit 38bb4feecf261d61dee9178fec585a0f563c88f4
    Author: geonolis <geonolis@hotmail.com>
    Date:   Sat Jun 1 20:39:14 2024 +0300
    
        Added backward compatibility CPT
    
        Added compatibility with woocommerce without HPOS activated
    
    commit 072db937dba26c7332ab4262164865049dbcf698
    Author: geonolis <geonolis@hotmail.com>
    Date:   Sat Jun 1 19:46:17 2024 +0300
    
        Update class-create-geniki-taxydromiki-vouchers-for-woo-v3-admin.php
    
        minor update to function gt_shipping_status( $order )
    
    commit 177059c0bf2cea2a6a754a608b1b4df36552e9a0
    Author: geonolis <geonolis@hotmail.com>
    Date:   Fri May 31 21:08:18 2024 +0300
    
        Update class-create-geniki-taxydromiki-vouchers-for-woo-v3-admin.php
    
        added echo shipping status results
    
    commit 3b6915576b7d1314c4337ad709f86b2c0e2af978
    Author: geonolis <geonolis@hotmail.com>
    Date:   Fri May 31 20:59:41 2024 +0300
    
        Update class-create-geniki-taxydromiki-vouchers-for-woo-v3-admin.php
    
    commit cba4bcde9bde51d9b612da33681469bb981265ca
    Author: geonolis <geonolis@hotmail.com>
    Date:   Fri May 31 20:08:07 2024 +0300
    
        testing getting voucher status
    
        new voucher was ok.
        now testing getting voucher status

--- public/class-create-geniki-taxydromiki-vouchers-for-woo-v3-public.php
+++ public/class-create-geniki-taxydromiki-vouchers-for-woo-v3-public.php
@@ -40,6 +40,16 @@ class Create_Geniki_Taxydromiki_Vouchers_For_Woo_V3_Public {
 	 */
 	private $version;
 
+
+	/**
+	 * The GT connection API object
+	 * It is referenced as class private property, to be created once and accessed from all class methods
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      GT_API    $gt_api    The API connection object.
+	 */
+	private GT_API $gt_api;
+
 	/**
 	 * Initialize the class and set its properties.
 	 *
@@ -60,7 +70,6 @@ class Create_Geniki_Taxydromiki_Vouchers_For_Woo_V3_Public {
 	 * @since    1.0.0
 	 */
 	public function enqueue_styles() {
-
 		/**
 		 * This function is provided for demonstration purposes only.
 		 *
@@ -72,9 +81,9 @@ class Create_Geniki_Taxydromiki_Vouchers_For_Woo_V3_Public {
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
-
+		$url = $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+		if ( strpos( $url, 'view-order'))
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/create-geniki-taxydromiki-vouchers-for-woo-v3-public.css', array(), $this->version, 'all' );
-
 	}
 
 	/**
@@ -83,7 +92,6 @@ class Create_Geniki_Taxydromiki_Vouchers_For_Woo_V3_Public {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts() {
-
 		/**
 		 * This function is provided for demonstration purposes only.
 		 *
@@ -95,44 +103,34 @@ class Create_Geniki_Taxydromiki_Vouchers_For_Woo_V3_Public {
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
-
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/create-geniki-taxydromiki-vouchers-for-woo-v3-public.js', array( 'jquery' ), $this->version, false );
-
 	}
-
 
 	/**
 	 * ΕΜΦΆΝΙΣΗ ΑΡΙΘΜΟΥ ΑΠΟΣΤΟΛΗΣ ΓΕΝΙΚΗΣ ΤΑΧΥΔΡΟΜΙΚΗΣ ΣΤΟ ΕΜΑΙΛ ΠΕΛΑΤΗ
 	 * @param $order
 	 */
 	function woocommerce_email_order_tracking( $order ) {
-
 		/* ανάκτηση αριθμού αποστολής από custom field */
 		$courier_voucher = $order->get_meta( 'courier_voucher') ;
-
 		/*  αν βρέθηκε αριθμός αποστολής εμφάνισε λινκ παρακολούθησης */
 		if ( ! empty( $courier_voucher) ) {
 			require_once 'partials/' . $this->plugin_name . '-client-email.php';
 		}
 	}
 
-
 	/**
 	 * ΕΜΦΆΝΙΣΗ ΑΡΙΘΜΟΥ ΑΠΟΣΤΟΛΗΣ ΓΕΝΙΚΗΣ ΤΑΧΥΔΡΟΜΙΚΗΣ ΣΤΟ ΛΟΓΑΡΙΑΣΜΟ ΠΕΛΑΤΗ
 	 * @param $order_id
 	 */
 	function woocommerce_view_order_tracking( $order_id ) {
-
 		/* ανάκτηση αριθμού αποστολής από custom field */
 		$courier_voucher = wc_get_order($order_id)->get_meta( 'courier_voucher') ;
-
 		/*  αν δεν έχει συμπληρωθεί αριθμός αποστολής επέστρεψε */
 		if ( ! empty( $courier_voucher) ) {
+			if ( ! @isset( $this->gt_api ) ) $this->gt_api = new GT_API();
 			require_once 'partials/' . $this->plugin_name . '-client-account.php';
 		}
-
 	}
-
-
 
 }
